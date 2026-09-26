ed3334aad2f266a80f7414bcbc55a162fec13cb4
Vasuki Patel <47256707+Vasuki8@users.noreply.github.com>
2026-09-26 12:21:35 -0400

    Recognize annual outlook communication sources

--- tracker/publication_coverage.py
+++ tracker/publication_coverage.py
@@ -18,7 +18,7 @@ COMMUNICATION_KINDS=("market view","unitholder letter")
 _COMMUNICATION_SOURCE=re.compile(
     r"newsletter|letter.*unitholder|unitholder.*letter|"
     r"market[\s_\-]*(?:outlook|update|view|insights?|commentar(?:y|ies))|equity[\s_\-]*outlook|"
-    r"latest[\s_\-]*commentar|investment[\s_\-]*view|cio[\s_\-]*(?:letter|view)|"
+    r"latest[\s_\-]*commentar|annual[\s_\-]*outlook|investment[\s_\-]*view|cio[\s_\-]*(?:letter|view)|"
     r"product[\s_\-]*note|presentation",
     re.I,
 )
